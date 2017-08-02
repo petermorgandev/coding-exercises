ee7845aa737b0679a0cc0f95b00878f44916a37b
Peter Morgan <codeup@1080peter.com>
2017-08-02 00:48:48 -0600

    Your -> That

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ new Vue ({
             this.dateOutput = '<div class="alert alert-danger">Sorry, that username was not found. Please try another username.</div>';
           }
           else if (data.created_time != null){
-            this.dateOutput = '<div class="alert alert-success">Your account was created on ' + moment(data.created_time).format("MMMM D, YYYY") + '.</div>';
+            this.dateOutput = '<div class="alert alert-success">That account was created on ' + moment(data.created_time).format("MMMM D, YYYY") + '.</div>';
           }
                       });
     }, 500) //end checkUsername
