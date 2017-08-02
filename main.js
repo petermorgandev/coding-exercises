c5ff38b48de79107e04df49cae1bc520d665ed36
Peter Morgan <codeup@1080peter.com>
2017-08-02 00:37:25 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+new Vue ({
+  el: '#app',
+  data: {
+    usernameInput: '',
+    dateOutput: ''
+  }, //end data
+  computed: {
+    htmlOutput: function () {
+      return this.dateOutput;
+    } //end htmlOutput
+  }, //end computed
+  methods: {
+    checkUsername: _.debounce(
+      function(e) {
+      this.usernameInput = e.target.value;
+      fetch('https://api.mixcloud.com/' + this.usernameInput + '/')
+        .then(res => res.json())
+        .then(data => {
+          if (data.created_time == null) {
+            this.dateOutput = '<div class="alert alert-danger">Sorry, that username was not found. Please try another username.</div>';
+          }
+          else if (data.created_time != null){
+            this.dateOutput = '<div class="alert alert-success">Your account was created on ' + moment(data.created_time).format("MMMM D, YYYY") + '.</div>';
+          }
+                      });
+    }, 500) //end checkUsername
+  }
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Mixcloud Anniversary
+This is a single page website that tells you the date when an account was created on [Mixcloud.com](https://www.mixcloud.com).
+
+Examples of valid usernames are `d`, `dj`, `djp`, `djpe`, `djpet`, `djpete`,`djpeter`, `djpetermorgan`.
+
+## Resources used:
+
+  - [Bootstrap V4 Alpha](http://v4-alpha.getbootstrap.com/)
+  - [Mixcloud API](https://www.mixcloud.com/developers/)
+  - [Moment.js](https://momentjs.com/)
+  - [Vue.js](https://vuejs.org/)
