4be563f7e59984179f2c4d582e2c1acfdbb51e57
Peter Morgan <codeup@1080peter.com>
2018-02-10 12:33:53 -0700

    Slight JS Refactor

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ new Vue ({
     checkUsername: _.debounce(
       function(e) {
       this.usernameInput = e.target.value;
-      fetch('https://api.mixcloud.com/' + this.usernameInput + '/')
+      fetch(`https://api.mixcloud.com/${this.usernameInput}/`)
         .then(res => res.json())
         .then(data => {
           if (data.created_time == null) {
-            this.dateOutput = '<div class="alert alert-danger">Sorry, that username was not found. Please try another username.</div>';
+            this.dateOutput = `<div class="alert alert-danger">Sorry, that username was not found. Please try another username.</div>`;
           }
           else if (data.created_time != null){
-            this.dateOutput = '<div class="alert alert-success">That account was created on ' + moment(data.created_time).format("MMMM D, YYYY") + '.</div>';
+            this.dateOutput = `<div class="alert alert-success">That account was created on ${moment(data.created_time).format("MMMM D, YYYY")}.</div>`;
           }
                       });
-    }, 500) //end checkUsername
+    }, 500)
   }
 });
