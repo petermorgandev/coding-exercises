d180a1059fee799acb4131aa24dc7d8590626ea7
Peter Morgan <codeup@1080peter.com>
2018-02-28 20:09:49 -0700

    Set default count to 0

--- jquery/app.js
+++ jquery/app.js
@@ -1,10 +1,10 @@
 let getInput = $('textarea');
-const getStrong = $('span')
+const getpan = $('span')
 
 function calculate() {
 	let innerInput =	getInput[0].value;
 	let calc = innerInput.length;
-	getStrong.text(calc);
+	getpan.text(calc);
 	}
 
 getInput.on('input', calculate);
