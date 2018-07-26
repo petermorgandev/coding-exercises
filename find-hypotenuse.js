e48e41cd023810341dd62b66c53bcd3c5c646d17
Peter Morgan <PeterMorganGH@users.noreply.github.com>
2018-07-26 13:16:27 -0600

    Update find-hypotenuse.js

--- find-hypotenuse.js
+++ find-hypotenuse.js
@@ -1,9 +1,8 @@
 function findHypotenuse(x, y) {
   if (typeof x !== 'number' || typeof y !== 'number') {
     return 'Sorry, but this requires 2 numbers. Please try again.';
-  } else {
-    return Math.sqrt((x * x) + (y * y)).toFixed(2);
-  }
+  } 
+  return Math.sqrt((x * x) + (y * y)).toFixed(2);
 }
 
 findHypotenuse(4, 2);
