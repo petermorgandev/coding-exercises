33478d463d84ec2c36599f7e9c55302c89cdc2a5
Peter Morgan <PeterMorganGH@users.noreply.github.com>
2018-07-26 12:47:09 -0600

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,8 +1,5 @@
-function anagram(x, y) {
-  let o = x.replace(/\s+/g, "").toLowerCase().split("").sort().join();
-  let p = y.replace(/\s+/g, "").toLowerCase().split("").sort().join();
+let sort = z => z.replace(/\s+/g, "").toLowerCase().split("").sort().join();
 
-  console.log(o === p);
-}
+let anagram = (x, y) => (sort(x) === sort(y));
 
 anagram("rail Safety", "fairy tales");
