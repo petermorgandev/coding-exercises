543b1ed1f3095672af421b58c91fbbcd5b912bf6
Peter Morgan <PeterMorganGH@users.noreply.github.com>
2018-07-30 08:02:53 -0600

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,13 +1,13 @@
 function palinCheck(x) {
+  let y;
   if (typeof x === "undefined" || x === null || x === "") {
     return "Sorry, but I need something to check. Please try again.";
   } else if (typeof x === "string") {
-    x = x.toLowerCase();
+    y = x.toLowerCase();
   } else if (typeof x === "number" || typeof x === "boolean") {
-    x = x.toString();
+    y = x.toString();
   }
-
-  return x === x.split("").reverse().join("") ? `${x} is the same forwards and backwards.` : `${x} is not the same forwards and backwards.`;
+  return y === y.split("").reverse().join("") ? `${x} is the same forwards and backwards.` : `${x} is not the same forwards and backwards.`;
 }
 
 palinCheck("Kayak");
