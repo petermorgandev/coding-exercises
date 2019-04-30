e6387518e57b100985973062c05bc275fb3beb01
Peter Morgan <codeup@1080peter.com>
2019-04-30 11:54:07 -0600

    added some fake data for devices

--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    devices: [], 
-    deviceCount: 0
+    devices: [],
+    deviceCount: 0,
+    fakeData: [
+      {
+        id: 0,
+        chart: 'chart',
+        tableData: [
+          { month: 'April', usage: 10, change: 'down' }, { month: 'March', usage: 12, change: 'down' }, { month: 'February', usage: 14, change: 'down' }, { month: 'January', usage: 20, change: '' }
+        ]
+      },
+      {
+        id: 1,
+        chart: 'chart',
+        tableData: [
+          { month: 'April', usage: 8, change: 'down' }, { month: 'March', usage: 10, change: 'down' }, { month: 'February', usage: 14, change: 'up' }, { month: 'January', usage: 11, change: '' }
+        ]
+      },
+      {
+        id: 2,
+        chart: 'chart',
+        tableData: [
+          { month: 'April', usage: 15, change: 'down' }, { month: 'March', usage: 18, change: 'down' }, { month: 'February', usage: 20, change: 'down' }, { month: 'January', usage: 21, change: '' }
+        ]
+      },
+      {
+        id: 3,
+        chart: 'chart',
+        tableData: [
+          { month: 'April', usage: 3, change: 'down' }, { month: 'March', usage: 4, change: 'down' }, { month: 'February', usage: 7, change: 'down' }, { month: 'January', usage: 8, change: '' }
+        ]
+      },
+      {
+        id: 4,
+        chart: 'chart',
+        tableData: [
+          { month: 'April', usage: 5, change: 'down' }, { month: 'March', usage: 6, change: 'down' }, { month: 'February', usage: 8, change: 'down' }, { month: 'January', usage: 9, change: '' }
+        ]
+      }
+    ]
   },
   mutations: {
-    addDeviceMutation (state, payload) {
+    addDeviceMutation(state, payload) {
       state.devices.push(payload);
       state.deviceCount++;
     }
