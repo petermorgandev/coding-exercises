b925244096699faaba693ce534393c18ae5c8cb1
Peter Morgan <codeup@1080peter.com>
2019-04-29 23:38:33 -0600

    removed some test data

--- src/store/store.js
+++ src/store/store.js
@@ -5,26 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    testString: 'this is a test string',
-    testArray: ['John', 'Steve', 'Bill'],
     devices: [
-      { id:1, floor: 'Upstairs', location: 'Bathroom', type: 'Toilet' },
+      /* { id:1, floor: 'Upstairs', location: 'Bathroom', type: 'Toilet' },
       { id:2, floor: 'Basement', location: 'Bathroom', type: 'Sink' },
       { id:3, floor: 'Upstairs', location: 'Bathroom', type: 'Shower' },
       { id:4, floor: 'Downstairs', location: 'Kitchen', type: 'Sink' },
       { id:5, floor: 'Downstairs', location: 'Bathroom', type: 'Toilet' },
-      { id:6, floor: 'Downstairs', location: 'Bathroom', type: 'Sink' }
-    ],
+      { id:6, floor: 'Downstairs', location: 'Bathroom', type: 'Sink' } */
+    ], 
     deviceCount: 0
   },
   mutations: {
-    addDevice: state => {
-      state.devices.push({ id: state.deviceCount++, floor: 'submitted', location: 'add.vue', type: 'button press' })
+    addDeviceMutation (state, payload) {
+      state.devices.push(payload)
     }
   },
   actions: {
-    addDevice: context => {
-      context.commit('addDevice')
+    addDeviceAction: (context, payload) => {
+      context.commit('addDeviceMutation', payload)
     }
   }
 })
