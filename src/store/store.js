f0ce5d1b503cbd7b3e7993b4542ecd8ea5b3dc6d
Peter Morgan <codeup@1080peter.com>
2019-04-29 21:49:44 -0600

    cleaned up store/js

--- src/store/store.js
+++ src/store/store.js
@@ -8,17 +8,18 @@ export default new Vuex.Store({
     testString: 'this is a test string',
     testArray: ['John', 'Steve', 'Bill'],
     devices: [
-      { name: 'Island', location: 'kitchen', type: 'sink' },
-      { name: 'Upstairs', location: 'bathroom', type: 'sink' },
-      { name: 'Upstairs', location: 'bathroom', type: 'toilet' },
-      { name: 'Upstairs', location: 'bathroom', type: 'shower' },
-      { name: 'Downstairs', location: 'bathroom', type: 'toilet' },
-      { name: 'Downstairs', location: 'bathroom', type: 'sink' }
-    ]
+      { id:1, floor: 'Upstairs', location: 'Bathroom', type: 'Toilet' },
+      { id:2, floor: 'Basement', location: 'Bathroom', type: 'Sink' },
+      { id:3, floor: 'Upstairs', location: 'Bathroom', type: 'Shower' },
+      { id:4, floor: 'Downstairs', location: 'Kitchen', type: 'Sink' },
+      { id:5, floor: 'Downstairs', location: 'Bathroom', type: 'Toilet' },
+      { id:6, floor: 'Downstairs', location: 'Bathroom', type: 'Sink' }
+    ],
+    deviceCount: 0
   },
   mutations: {
     addDevice: state => {
-      state.devices.push({ name: 'submitted', location: 'add.vue', type: 'button press' })
+      state.devices.push({ id: state.deviceCount++, floor: 'submitted', location: 'add.vue', type: 'button press' })
     }
   },
   actions: {
