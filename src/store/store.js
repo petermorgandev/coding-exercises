78a878f1078e7019f45875ad32bfe8d2de07d42b
Peter Morgan <codeup@1080peter.com>
2019-04-30 20:45:56 -0600

    changed fake table data

--- src/store/store.js
+++ src/store/store.js
@@ -12,35 +12,35 @@ export default new Vuex.Store({
         id: 0,
         chart: 'chart',
         tableData: [
-          { month: 'April', usage: 10, change: 'down' }, { month: 'March', usage: 12, change: 'down' }, { month: 'February', usage: 14, change: 'down' }, { month: 'January', usage: 20, change: '' }
+          { month: 'April', usage: 10, change: 'down' }, { month: 'March', usage: 12, change: 'down' }, { month: 'February', usage: 14, change: 'down' }, { month: 'January', usage: 20, change: 'Device Installed' }
         ]
       },
       {
         id: 1,
         chart: 'chart',
         tableData: [
-          { month: 'April', usage: 8, change: 'down' }, { month: 'March', usage: 10, change: 'down' }, { month: 'February', usage: 14, change: 'up' }, { month: 'January', usage: 11, change: '' }
+          { month: 'April', usage: 8, change: 'down' }, { month: 'March', usage: 10, change: 'down' }, { month: 'February', usage: 14, change: 'up' }, { month: 'January', usage: 11, change: 'Device Installed' }
         ]
       },
       {
         id: 2,
         chart: 'chart',
         tableData: [
-          { month: 'April', usage: 15, change: 'down' }, { month: 'March', usage: 18, change: 'down' }, { month: 'February', usage: 20, change: 'down' }, { month: 'January', usage: 21, change: '' }
+          { month: 'April', usage: 15, change: 'down' }, { month: 'March', usage: 18, change: 'down' }, { month: 'February', usage: 20, change: 'down' }, { month: 'January', usage: 21, change: 'Device Installed' }
         ]
       },
       {
         id: 3,
         chart: 'chart',
         tableData: [
-          { month: 'April', usage: 3, change: 'down' }, { month: 'March', usage: 4, change: 'down' }, { month: 'February', usage: 7, change: 'down' }, { month: 'January', usage: 8, change: '' }
+          { month: 'April', usage: 3, change: 'down' }, { month: 'March', usage: 4, change: 'down' }, { month: 'February', usage: 7, change: 'down' }, { month: 'January', usage: 8, change: 'Device Installed' }
         ]
       },
       {
         id: 4,
         chart: 'chart',
         tableData: [
-          { month: 'April', usage: 5, change: 'down' }, { month: 'March', usage: 6, change: 'down' }, { month: 'February', usage: 8, change: 'down' }, { month: 'January', usage: 9, change: '' }
+          { month: 'April', usage: 5, change: 'down' }, { month: 'March', usage: 6, change: 'down' }, { month: 'February', usage: 8, change: 'down' }, { month: 'January', usage: 9, change: 'Device Installed' }
         ]
       }
     ]
