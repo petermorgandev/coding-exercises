15d6b32e4bfe87143ec3cd999207e7323deca0f8
Peter Morgan <codeup@1080peter.com>
2019-04-30 20:46:46 -0600

    initial formatting of devices page

--- src/views/Devices.vue
+++ src/views/Devices.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="devices">
     <Navbar/>
-    <div class="card" v-for="device in devices" v-bind:key="device.id">
-      <div class="card-body">
-        <h5 class="card-title">{{device.floor}} {{device.room}} {{device.connectedTo}}</h5>
-        <a :href="`#/device/${device.id}`" class="card-link btn btn-primary">Device Info</a>
+    <div class="container bg-white">
+      <div class="row py-4">
+        <div class="col">
+          <h1>Connected Devices</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 mb-4" v-for="device in devices" v-bind:key="device.id">
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <ul class="list-group mb-3 list-group-flush">
+                <li class="list-group-item">Floor: {{device.floor}}</li>
+                <li class="list-group-item">Room: {{device.room}}</li>
+                <li
+                  class="list-group-item"
+                  style="border-bottom:none"
+                >Connected To: {{device.connectedTo}}</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <a
+                  :href="`#/device/${device.id}`"
+                  class="card-link btn btn-sm btn-custom"
+                >Device Info</a>
+                <small class="text-success">Connected</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
