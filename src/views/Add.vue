b8696c22ee7036585eece03bb6d4993180099ee6
Peter Morgan <codeup@1080peter.com>
2019-04-30 20:46:28 -0600

    added heading to add.vue

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,39 +1,50 @@
 <template>
   <div class="add">
     <Navbar/>
-    <form>
-      <div class="form-group">
-        <label for="floor">Floor:</label>
-        <select v-model="floor" id="floor" class="custom-select">
-          <option value="Basement" selected>Basement</option>
-          <option value="Ground Floor">Ground Floor</option>
-          <option value="Second Floor">Second Floor</option>
-          <option value="Attic">Attic</option>
-        </select>
+    <div class="container bg-white">
+      <div class="row py-4">
+        <div class="col">
+          <h1>Add New Device</h1>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="room">Room:</label>
-        <select v-model="room" id="room" class="custom-select">
-          <option value="Bathroom" selected>Bathroom</option>
-          <option value="Garage">Garage</option>
-          <option value="Kitchen">Kitchen</option>
-          <option value="Laundry Room">Laundry Room</option>
-        </select>
+      <div class="row">
+        <form class="col">
+          <div class="form-group">
+            <label for="floor">Floor:</label>
+            <select v-model="floor" id="floor" class="custom-select">
+              <option value="Basement" selected>Basement</option>
+              <option value="Ground Floor">Ground Floor</option>
+              <option value="Second Floor">Second Floor</option>
+              <option value="Attic">Attic</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="room">Room:</label>
+            <select v-model="room" id="room" class="custom-select">
+              <option value="Bathroom" selected>Bathroom</option>
+              <option value="Garage">Garage</option>
+              <option value="Kitchen">Kitchen</option>
+              <option value="Laundry Room">Laundry Room</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="connectedTo">Connected to:</label>
+            <select v-model="connectedTo" id="connectedTo" class="custom-select">
+              <option value="Shower" selected>Shower</option>
+              <option value="Sink">Sink</option>
+              <option value="Toilet">Toilet</option>
+              <option value="Washing Machine">Washing Machine</option>
+            </select>
+          </div>
+          <div v-if="count < 5">
+            <button v-on:click="addDeviceMethod" class="btn btn-custom mb-4">Submit</button>
+          </div>
+          <div v-else>
+            <button class="btn btn-danger mb-4" disabled>Device Limit Reached</button>
+          </div>
+        </form>
       </div>
-      <div class="form-group">
-        <label for="connectedTo">Connected to:</label>
-        <select v-model="connectedTo" id="connectedTo" class="custom-select">
-          <option value="Shower" selected>Shower</option>
-          <option value="Sink">Sink</option>
-          <option value="Toilet">Toilet</option>
-          <option value="Washing Machine">Washing Machine</option>
-        </select>
-      </div>
-      <div v-if='count < 5' >
-        <button v-on:click="addDeviceMethod" class="btn btn-primary">Submit</button>
-      </div>
-      <div v-else><button class="btn btn-danger" disabled>Device Limit Reached</button></div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -69,3 +80,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+form {
+  height: 100%;
+}
+</style>
