2a8c37aaa7b0b7e8f283a2125702be00340a7c58
Petermorgangh <codeup@1080peter.com>
2019-05-02 15:44:31 -0600

    updated stlying on device and devices pages

--- src/views/Device.vue
+++ src/views/Device.vue
@@ -8,16 +8,37 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-12">
-          <p><strong>Floor:</strong> {{deviceData.floor}}</p>
-          <p><strong>Room:</strong> {{deviceData.room}}</p>
-          <p><strong>Connected To:</strong> {{deviceData.connectedTo}}</p>
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-group mb-3 list-group-flush">
+                <li class="list-group-item">
+                  <h4>Floor:</h4>
+                  <p class="lead">{{deviceData.floor}}</p>
+                </li>
+                <li class="list-group-item">
+                  <h4>Room:</h4>
+                  <p class="lead">{{deviceData.room}}</p>
+                </li>
+                <li class="list-group-item" style="border-bottom:none">
+                  <h4>Connected To:</h4>
+                  <p class="lead">{{deviceData.connectedTo}}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
       <div class="row">
         <div class="col-sm-12">
-          <h2>Weekly Usage</h2>
-          <chartist type="Line" ratio="ct-major-twelfth" :data="chartData" :options="chartOptions" class="mb-4"></chartist>
+          <h2 class>Weekly Usage</h2>
+          <chartist
+            type="Line"
+            ratio="ct-major-twelfth"
+            :data="chartData"
+            :options="chartOptions"
+            class="my-4"
+          ></chartist>
         </div>
       </div>
       <div class="row">
@@ -73,3 +94,13 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.bg-boxes {
+  background-color: #0f434f;
+}
+
+.card {
+  border: none;
+}
+</style>
--- src/views/Devices.vue
+++ src/views/Devices.vue
@@ -13,21 +13,31 @@
             <h3>You have not added any devices.</h3>
           </div>
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-4 mb-4" v-for="device in devices" v-bind:key="device.id">
+        <div
+          class="col-sm-12 col-md-6 col-lg-6 col-xl-4 mb-4"
+          v-for="device in devices"
+          v-bind:key="device.id"
+        >
           <div class="card shadow-sm">
             <div class="card-body">
               <ul class="list-group mb-3 list-group-flush">
-                <li class="list-group-item"><strong>Floor:</strong> {{device.floor}}</li>
-                <li class="list-group-item"><strong>Room:</strong> {{device.room}}</li>
-                <li
-                  class="list-group-item"
-                  style="border-bottom:none"
-                ><strong>Connected To:</strong> {{device.connectedTo}}</li>
+                <li class="list-group-item">
+                  <h4>Floor:</h4>
+                  <p class="lead">{{device.floor}}</p>
+                </li>
+                <li class="list-group-item">
+                  <h4>Room:</h4>
+                  <p class="lead">{{device.room}}</p>
+                </li>
+                <li class="list-group-item" style="border-bottom:none">
+                  <h4>Connected To:</h4>
+                  <p class="lead">{{device.connectedTo}}</p>
+                </li>
               </ul>
               <div class="d-flex justify-content-between align-items-center">
                 <a
                   :href="`#/device/${device.id}`"
-                  class="card-link btn btn-sm btn-custom"
+                  class="card-link btn btn-lg btn-custom"
                 >Device Info</a>
                 <small class="text-success">Connected</small>
               </div>
