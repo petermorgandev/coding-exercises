cb97c664f0971ef10804794e3fb1c91a4ff62151
Petermorgangh <codeup@1080peter.com>
2019-05-05 23:49:07 -0600

    Removed extra "p" character in Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col">
         <h3 class="mt-4">What is 143chars?</h3>
-        <p>p 143chars is totally not a twitter clone. It is a project to learn more about Node.js, Express, and MongoDB.</p>
+        <p>143chars is totally not a twitter clone. It is a project to learn more about Node.js, Express, and MongoDB.</p>
       </div>
     </div>
   </div>
