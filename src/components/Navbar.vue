cc3f61738c9c4f7b9a21eaea18db64fccd1ec5dc
Petermorgangh <codeup@1080peter.com>
2019-05-05 23:46:27 -0600

    Created Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bg-dark">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">143chars</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li
+              class="nav-item"
+              v-for="link in navbarLinks"
+              :to="link.path"
+              v-bind:key="link.index"
+            >
+              <router-link :to="link.path" class="nav-link">{{link.text}}</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      navbarLinks: [
+        { path: "/register", text: "Register" },
+        { path: "/profile", text: "Profile" },
+        { path: "/new", text: "New Msg" },
+        { path: "settings", text: "Settings" },
+        { path: "logout", text: "Log Out" },
+        { path: "login", text: "Log In" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
