e7d1ffb701374bd0ab159c42a8f6416b8c88b94b
Petermorgangh <codeup@1080peter.com>
2019-05-06 09:25:44 -0600

    Log In Page:
    - Added route to router.js
    - Created initial login.vue file

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from './views/Index.vue'
 import Register from './views/Register.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h3 class="mt-4">Log In</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <form action="/login" method="POST">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter desired username"
+              v-model="usernameInput"
+              class="form-control"
+            >
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter desired password"
+              v-model="passwordInput"
+              class="form-control"
+            >
+          </div>
+          <div class="form-group"></div>
+          <button class="btn btn-primary" @click="login">Log In</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+
+export default {
+  data() {
+    return {
+      usernameInput: "",
+      passwordInput: ""
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        await AuthenticationService.register({
+          usernameInput: this.usernameInput,
+          passwordInput: this.passwordInput,
+          avatarInput: this.avatarInput
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
