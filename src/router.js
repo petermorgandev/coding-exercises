da6123c53ab37de491829b2765e11879c4a0920c
Petermorgangh <codeup@1080peter.com>
2019-05-05 23:16:40 -0600

    Deleted home.vue and helloword.vue

--- src/router.js
+++ src/router.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Register from './views/Register.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/register',
       name: 'register',
