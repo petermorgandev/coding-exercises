5dab38bea0cdde1bda33bae62cac72633eb0cca9
Petermorgangh <codeup@1080peter.com>
2019-05-05 22:36:32 -0600

    Added basic registration via API

---
+++ src/views/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="text" name="username" placeholder v-model="usernameInput">
+    <br>
+    <input type="password" name="password" id v-model="passwordInput">
+    <br>
+    <input type="text" name id v-model="avatarInput">
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+
+export default {
+  data() {
+    return {
+      usernameInput: "",
+      passwordInput: "",
+      avatarInput: "coolshades"
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        await AuthenticationService.register({
+          usernameInput: this.usernameInput,
+          passwordInput: this.passwordInput,
+          avatarInput: this.avatarInput
+        })
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
