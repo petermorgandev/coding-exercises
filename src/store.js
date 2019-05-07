ac1d4249753f83f3bfdfbf3e00cf9836a6064e03
Petermorgangh <codeup@1080peter.com>
2019-05-07 12:41:13 -0600

    Set up vueX store.js

--- src/store.js
+++ src/store.js
@@ -5,12 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: null,
+    isUserLoggedIn: false
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user;
+      if (user) {
+        state.isUserLoggedIn = true;
+      } else {
+        state.isUserLoggedIn = false;
+      }
+    }
   },
   actions: {
-
+    setUser({ commit }, user) {
+      commit('setUser', user);
+    }
   }
 })
