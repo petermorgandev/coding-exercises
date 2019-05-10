10c7346c038bade682d2d61a8dda33832f205214
Petermorgangh <codeup@1080peter.com>
2019-05-09 23:48:53 -0600

    Styling on Settings page matches base 143chars

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -5,33 +5,41 @@
         <h3 class="my-4">Settings</h3>
       </div>
     </div>
-    <div class="row mb-4">
-      <div class="col border border-success rounded-lg py-3">
-        <h4 class="mb-3">User Options</h4>
-        <form>
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" id="username" v-model="usernameInput" class="form-control">
-          </div>
-          <div class="form-group">
-            <label for="avatar">Avatar</label>
-            <select name="avatarInput" v-model="avatarInput" id="avatar" class="form-control">
-              <option v-for="avatar in avatarOptions" v-bind:key="avatar.index">{{avatar}}</option>
-            </select>
-          </div>
-          <button class="btn btn-primary" @click.prevent="updateUserSettings">Update</button>
-        </form>
-      </div>
-    </div>
     <div class="row">
-      <div class="col border border-danger rounded-lg py-3">
-        <h4 class="mb-3">Danger Zone</h4>
-        <p>These options are irreversible!</p>
-        <a :href="`/api/delete/messages/${$store.state.user}`" class="btn btn-danger">Delete ALL Messages</a> &nbsp;
-        <a :href="`/api/delete/user/${$store.state.user}`" class="btn btn-danger">Delete Acount</a>
+      <div class="col-md-6 p-1 mb-4 ">
+        <div class="border border-success rounded-lg p-3">
+          <h4 class="mb-3">User Options</h4>
+          <form>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" id="username" v-model="usernameInput" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="avatar">Avatar</label>
+              <select name="avatarInput" v-model="avatarInput" id="avatar" class="form-control">
+                <option v-for="avatar in avatarOptions" v-bind:key="avatar.index">{{avatar}}</option>
+              </select>
+            </div>
+            <button class="btn btn-primary" @click.prevent="updateUserSettings">Update</button>
+          </form>
+        </div>
+      </div>
+
+      <div class="col-md-6 p-1">
+        <div class="border border-danger rounded-lg p-3">
+          <h4 class="mb-3">Danger Zone</h4>
+          <p>These options are irreversible!</p>
+          <a
+            :href="`/api/delete/messages/${$store.state.user}`"
+            class="btn btn-danger"
+          >Delete All Messages</a>&nbsp;
+          <a
+            :href="`/api/delete/user/${$store.state.user}`"
+            class="btn btn-danger"
+          >Delete Account</a>
+        </div>
       </div>
     </div>
-    {{user}}
   </div>
 </template>
 
@@ -55,7 +63,7 @@ export default {
       ],
       usernameInput: "",
       avatarInput: "",
-      user: ''
+      user: ""
     };
   },
   methods: {
@@ -65,7 +73,10 @@ export default {
           usernameInput: this.usernameInput,
           avatarInput: this.avatarInput
         };
-        await AuthenticationService.updateUserSettings(this.$store.state.user, data);
+        await AuthenticationService.updateUserSettings(
+          this.$store.state.user,
+          data
+        );
         this.$router.push({ name: "home" });
       } catch (error) {
         this.error = error.response.data.error;
@@ -73,11 +84,12 @@ export default {
     }
   },
   async created() {
-    const user = await AuthenticationService.getUserSettings(this.$store.state.user);
+    const user = await AuthenticationService.getUserSettings(
+      this.$store.state.user
+    );
     const { username, avatar } = user.data[0];
     this.avatarInput = avatar;
     this.usernameInput = username;
-
   }
 };
 </script>
