d705c50b792230aacdbbabe47c5aa264ec0ecf45
Petermorgangh <codeup@1080peter.com>
2019-05-09 20:37:18 -0600

    Added remaining to AuthenticationService.js

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -1,7 +1,44 @@
 import Api from './Api'
 
 export default {
-  register (credentials) {
-    return Api().post('api/new/user', credentials)
+
+  getAll(){
+    return Api().get('/')
+  },
+
+  getUserMessages(userId){
+    return Api().get(`/user/${userId}`)
+  },
+
+  getUserSettings(userId){
+    return Api().get(`/user/${userId}/settings`)
+  },
+
+  updateUserSettings(userId, data){
+    return Api().put(`/user/${userId}/settings/update`, data)
+  },
+
+  deleteMsg(messageId){
+    return Api().delete(`/delete/message/${messageId}`)
+  },
+
+  deleteAllMessages(userId){
+    return Api().delete(`/delete/messages/${userId}`)
+  },
+
+  deleteUser(userId){
+    return Api().delete(`/delete/user/${userId}`)
+  },
+
+  register(credentials) {
+    return Api().post('/new/user', credentials);
+  },
+  
+  newMsg(msgData){
+    return Api().post('/new/message', msgData)
+  },
+  
+  login(credentials) {
+    return Api().post('/login', credentials)
   }
 }
