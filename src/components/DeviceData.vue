9f64df3a08e8c18a0ff96643002f491a5bcda83c
Petermorgangh <codeup@1080peter.com>
2019-05-15 09:29:24 -0600

    Moved DeviceData into component

---
+++ src/components/DeviceData.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <div class="card">
+        <div class="card-body">
+          <ul class="list-group mb-3 list-group-flush">
+            <li class="list-group-item">
+              <h4>Floor:</h4>
+              <p class="lead">{{deviceData.floor}}</p>
+            </li>
+            <li class="list-group-item">
+              <h4>Room:</h4>
+              <p class="lead">{{deviceData.room}}</p>
+            </li>
+            <li class="list-group-item" style="border-bottom:none">
+              <h4>Connected To:</h4>
+              <p class="lead">{{deviceData.connectedTo}}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeviceData",
+  props: {
+    deviceData: Object
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  border: none;
+}
+</style>
--- src/views/Device.vue
+++ src/views/Device.vue
@@ -3,28 +3,7 @@
     <Navbar/>
     <div class="container bg-white">
       <PageHeader>Device Info</PageHeader>
-      <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <ul class="list-group mb-3 list-group-flush">
-                <li class="list-group-item">
-                  <h4>Floor:</h4>
-                  <p class="lead">{{deviceData.floor}}</p>
-                </li>
-                <li class="list-group-item">
-                  <h4>Room:</h4>
-                  <p class="lead">{{deviceData.room}}</p>
-                </li>
-                <li class="list-group-item" style="border-bottom:none">
-                  <h4>Connected To:</h4>
-                  <p class="lead">{{deviceData.connectedTo}}</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DeviceData :deviceData='deviceData' />
       <div class="row">
         <div class="col-sm-12">
           <h2 class>Weekly Usage</h2>
@@ -46,6 +25,7 @@
 // @ is an alias to /src
 import Navbar from "@/components/Navbar";
 import PageHeader from "@/components/PageHeader";
+import DeviceData from "@/components/DeviceData";
 import MonthlyUsage from "@/components/MonthlyUsage";
 export default {
   data() {
@@ -70,6 +50,7 @@ export default {
   components: {
     Navbar,
     PageHeader,
+    DeviceData,
     MonthlyUsage
   },
   created() {
@@ -90,9 +71,5 @@ export default {
 <style scoped>
 .bg-boxes {
   background-color: #0f434f;
-}
-
-.card {
-  border: none;
 }
 </style>
