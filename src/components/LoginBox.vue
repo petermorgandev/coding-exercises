96a06f82d4f9a795f18036db75cd95a1194bacf7
Petermorgangh <codeup@1080peter.com>
2019-05-15 08:28:36 -0600

    Moved Login content to seperate component file

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <div class="loginBox mt-5 pt-5 px-3">
+          <img alt="logo" src="../assets/pipe.svg" class="mb-5">
+          <h1 class="h3 mb-4 font-weight-normal">Please sign in</h1>
+          <form class="p-3">
+            <input type="text" placeholder="Username" class="form-control">
+            <input type="password" placeholder="Password" class="form-control mb-4">
+            <a href="#/add" class="btn btn-lg btn-custom btn-block">Log In</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginBox"
+};
+</script>
+
+<style scoped>
+form {
+  width: 100%;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+}
+
+.loginBox {
+  background-color: white;
+  max-width: 350px;
+  margin: auto;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,14 @@
 <template>
-  <div class="login text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="loginBox mt-5 pt-5 px-3">
-            <img alt="logo" src="../assets/pipe.svg" class="mb-5">
-            <h1 class="h3 mb-4 font-weight-normal">Please sign in</h1>
-            <form class="p-3">
-              <input type="text" placeholder="Username" class="form-control">
-              <input type="password" placeholder="Password" class="form-control mb-4">
-              <a href="#/add" class="btn btn-lg btn-custom btn-block">Log In</a>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <LoginBox />
 </template>
 
 <script>
+import LoginBox from '@/components/LoginBox';
+
 export default {
-  name: "login"
+  name: "login",
+  components: {
+      LoginBox
+    }
 };
 </script>
-
-<style scoped>
-form {
-  width: 100%;
-}
-
-img {
-  width: 200px;
-  height: 200px;
-}
-
-.loginBox {
-  background-color: white;
-  max-width: 350px;
-  margin: auto;
-}
-</style>
