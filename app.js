6c1d3dd7a68f85e6a3b90cac7ef0537636a7aea8
Petermorgangh <codeup@1080peter.com>
2019-05-15 22:02:16 -0600

    Refactored onclick events to event listeners

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-function listify (mode) {
+function listify(mode) {
   let newText = [],
     textBox = document.getElementById('textarea'),
     n = textBox.value.split("\n");
-    
+
   for (var x in n) {
-    if (mode){
+    if (mode) {
       newText[x] = `\t <li>${n[x]}</li>`;
     } else {
       newText[x] = `<li>${n[x]}</li>`;
     }
   }
 
-  if (mode){
+  if (mode) {
     newText.unshift(`<${mode}>`);
     newText.push(`</${mode}>`);
   }
 
   textBox.value = newText.join("\r\n");
 };
+
+document.getElementById('liOnly').addEventListener('click',  () => { listify() });
+
+document.getElementById('ulLi').addEventListener('click', () => { listify('ul') });
+
+document.getElementById('olLi').addEventListener('click',  () => {listify('ol')});
