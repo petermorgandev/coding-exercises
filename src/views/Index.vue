a9905c28fe9e2c722eac1ed6f9b7a7bbfcbb61d1
Petermorgangh <codeup@1080peter.com>
2019-05-18 11:56:03 -0600

    Updated text on logged out homepage

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,10 @@
     <PageHeader>What is 143chars?</PageHeader>
     <div class="row">
       <div class="col">
-        <p>143chars is totally not a twitter clone. It is a project to learn more about Node.js, Express, and MongoDB.</p>
+        <p>
+          143chars-vue is a Vue.js client for the
+          <a href="https://github.com/PeterMorganGH/143chars#api">143chars REST API</a>. It has the same functionality as the 143chars front end. However, it uses the REST API for database communication and Vue.js instead of vanilla JavaScript.
+        </p>
       </div>
     </div>
   </div>
