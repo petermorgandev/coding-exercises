540ecf672837227360c3ad1097f491ed90da9115
Petermorgangh <codeup@1080peter.com>
2019-05-18 11:19:52 -0600

    Moved messages on home view to component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,19 @@
 <template>
   <div>
     <PageHeader>Recent Messages</PageHeader>
-    <div class="row">
-      <div class="col">
-        <ul class="list-unstyled mt-2">
-          <li class="media mb-4" v-for="message in messages.data" v-bind:key="message.index">
-            <img :src="`avatars/${message.user.avatar}.svg`" width="64" height="64" class="mr-3">
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">{{message.message}}</h5>Posted by
-              <a :href="'#/profile/' + message.user._id">@{{message.user.username}}</a>&nbsp;
-              <span
-                :title="moment(message.date).format('MMMM D, YYYY h:mma')"
-              >{{moment(message.date).fromNow()}}</span>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <HomeMessages :messages="messages.data"/>
   </div>
 </template>
 
 <script>
 import AuthenticationService from "@/services/AuthenticationService";
 import PageHeader from "@/components/PageHeader";
+import HomeMessages from "@/components/HomeMessages";
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+    HomeMessages
   },
   data() {
     return {
