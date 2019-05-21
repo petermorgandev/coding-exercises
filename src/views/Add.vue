d3b25502bfe6af8c54b1de576b4508d0c494529c
Petermorgangh <codeup@1080peter.com>
2019-05-21 10:52:57 -0600

    Moved add device form into its own component

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -6,43 +6,7 @@
         Add New Device
         <small class="text-muted">{{count}}/6 devices connected</small>
       </PageHeader>
-      <div class="row">
-        <form class="col">
-          <div class="form-group">
-            <label for="floor">Floor:</label>
-            <select v-model="floor" id="floor" class="custom-select">
-              <option value="Basement" selected>Basement</option>
-              <option value="Ground Floor">Ground Floor</option>
-              <option value="Second Floor">Second Floor</option>
-              <option value="Attic">Attic</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="room">Room:</label>
-            <select v-model="room" id="room" class="custom-select">
-              <option value="Bathroom" selected>Bathroom</option>
-              <option value="Garage">Garage</option>
-              <option value="Kitchen">Kitchen</option>
-              <option value="Laundry Room">Laundry Room</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="connectedTo">Connected to:</label>
-            <select v-model="connectedTo" id="connectedTo" class="custom-select">
-              <option value="Shower" selected>Shower</option>
-              <option value="Sink">Sink</option>
-              <option value="Toilet">Toilet</option>
-              <option value="Washing Machine">Washing Machine</option>
-            </select>
-          </div>
-          <div v-if="count < 6">
-            <button v-on:click.prevent="addDeviceMethod" class="btn btn-custom mb-4">Submit</button>
-          </div>
-          <div v-else>
-            <button class="btn btn-danger mb-4" disabled>Device Limit Reached</button>
-          </div>
-        </form>
-      </div>
+      <AddDeviceForm/>
     </div>
   </div>
 </template>
@@ -50,40 +14,19 @@
 <script>
 import Navbar from "@/components/Navbar";
 import PageHeader from "@/components/PageHeader";
+import AddDeviceForm from "@/components/AddDeviceForm";
 
 export default {
   name: "add",
   components: {
     Navbar,
-    PageHeader
-  },
-  data() {
-    return {
-      floor: "Basement",
-      room: "Bathroom",
-      connectedTo: "Shower"
-    };
+    PageHeader,
+    AddDeviceForm
   },
   computed: {
     count() {
       return this.$store.state.deviceCount;
     }
-  },
-  methods: {
-    addDeviceMethod: function() {
-      this.$store.dispatch("addDeviceAction", {
-        id: this.count,
-        floor: this.floor,
-        room: this.room,
-        connectedTo: this.connectedTo
-      });
-    }
   }
 };
 </script>
-
-<style scoped>
-form {
-  height: 100%;
-}
-</style>
