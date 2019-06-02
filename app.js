449284037d1047c5b31009e1f5c71f886b4b19a1
Petermorgangh <codeup@1080peter.com>
2019-06-01 20:18:17 -0600

    Refactor app.js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 let convert = new showdown.Converter()
 
-$('#submitButton').on('click', event => {
+const addJot = event => {
   event.preventDefault();
-  let content =  $(".textarea");
-  const tileParent = $('#tileParent');
-  const templateStart = `<div class="tile is-child box"><a href="#" class="button is-danger is-outlined is-small is-pulled-right deleteButton">Delete</a>`;
-  const templateEnd = '</p></div>'
-  let markedContent = convert.makeHtml(content.val());
-  tileParent.append(`${templateStart}${markedContent}${templateEnd}`);
-  content.val('');
-});
+  const templateStart = `<div class="tile is-child box">
+    <a href="#" class="button is-danger is-outlined is-small is-pulled-right deleteButton">Delete</a>`;
+  const templateEnd = '</p></div>';
+  let markedContent = convert.makeHtml($(".textarea").val());
+  $('#tileParent').append(`${templateStart}${markedContent}${templateEnd}`);
+  $(".textarea").val('');
+};
 
-$('#tileParent').on('click', 'a.deleteButton', event => {
+const removeJot = event => {
   event.preventDefault();
   $(event.target).closest('div').remove();
-});
+};
+
+$('#submitButton').on('click', addJot);
+
+$('#tileParent').on('click', 'a.deleteButton', removeJot);
