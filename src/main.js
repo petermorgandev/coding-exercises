cb40e33de94dbb1bc4855c4acbca1fb26ce6ef6c
Petermorgangh <peter@1080peter.com>
2019-06-03 12:02:28 -0600

    Linked to bootstrap instead of including

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
-// import 'bootstrap'
-import "bootstrap/dist/css/bootstrap.min.css";
 require("./assets/style.css");
 
 Vue.use(require("vue-chartist"));
