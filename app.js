dc43f52732a850c27ef349b9eab55475d03fffc2
Petermorgangh <peter@1080peter.com>
2019-06-08 18:13:48 -0600

    Initial Code Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const addJot = event => {
+  const convert = new showdown.Converter();
+  const textBox = document.getElementById("textBox");
+  const tileParent = document.getElementById("tileParent");
+  event.preventDefault();
+  let markedContent = convert.makeHtml(textBox.value);
+  const template = 
+    `<div class="tile is-child box content">
+      <a href="#" class="button is-danger is-outlined is-small is-pulled-right deleteButton">Delete</a>
+      ${markedContent}
+    </div>`;
+  tileParent.insertAdjacentHTML("beforeend", template);
+  textBox.value = "";
+};
+
+const removeJot = event => {
+  event.preventDefault();
+  event.target.closest('div').remove();
+};
+
+document.getElementById("submitButton").addEventListener("click", addJot);
+
+document.getElementById("tileParent").addEventListener("click", removeJot);
