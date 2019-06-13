3b9e0eb974fe6d8e2a76776ac165ca4931795d3a
Petermorgangh <peter@1080peter.com>
2019-06-12 23:02:22 -0600

    Fixed issue with css output

--- src/components/CSSOutput.vue
+++ src/components/CSSOutput.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="p-4 rounded-lg bg-light">
     <pre class><code><span class="cssSelector">{{styling.className}}</span> {
-  <span class="cssProperty">font-size</span>: <span class="cssPropertyValue">{{styling.fontSize}}px</span>,
-  <span class="cssProperty">padding</span>: <span class="cssPropertyValue">{{styling.vPadding}}px {{styling.hPadding}}px</span>,
-  <span class="cssProperty">border-radius</span>: <span class="cssPropertyValue">{{styling.borderRadius}}%</span>,
-  <span class="cssProperty">border-width</span>: <span class="cssPropertyValue">{{styling.borderSize}}px</span>,
-  <span class="cssProperty">border-style</span>: <span class="cssPropertyValue">{{styling.borderStyle}}</span>,
-  <span class="cssProperty">border-color</span>: <span class="cssPropertyValue">{{styling.borderColor}}</span>,
-  <span class="cssProperty">color</span>: <span class="cssPropertyValue">{{styling.fontColor}}</span>,
-  <span class="cssProperty">background-color</span>: <span class="cssPropertyValue">{{styling.bgColor}}</span>
+  <span class="cssProperty">font-size</span>: <span class="cssPropertyValue">{{styling.fontSize}}px</span>;
+  <span class="cssProperty">padding</span>: <span class="cssPropertyValue">{{styling.vPadding}}px {{styling.hPadding}}px</span>;
+  <span class="cssProperty">border-radius</span>: <span class="cssPropertyValue">{{styling.borderRadius}}%</span>;
+  <span class="cssProperty">border-width</span>: <span class="cssPropertyValue">{{styling.borderSize}}px</span>;
+  <span class="cssProperty">border-style</span>: <span class="cssPropertyValue">{{styling.borderStyle}}</span>;
+  <span class="cssProperty">border-color</span>: <span class="cssPropertyValue">{{styling.borderColor}}</span>;
+  <span class="cssProperty">color</span>: <span class="cssPropertyValue">{{styling.fontColor}}</span>;
+  <span class="cssProperty">background-color</span>: <span class="cssPropertyValue">{{styling.bgColor}}</span>;
 }</code></pre>
   </div>
 </template>
