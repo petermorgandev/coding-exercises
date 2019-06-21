42c7f67988c8724e17f83e2252e9a3d4d010b8aa
Petermorgangh <peter@1080peter.com>
2019-06-20 20:18:59 -0600

    Infinite scrolling works

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
+## Notes
+
+- Here is an example of the JSON you can get for a subreddit
+
+  - https://www.reddit.com/r/aww/hot/.json
+  - https://www.reddit.com/r/aww/new/.json
+  - https://www.reddit.com/r/aww/controversial/.json
+  - https://www.reddit.com/r/aww/top/.json?t=hour
+  - https://www.reddit.com/r/aww/top/.json?t=day
+  - https://www.reddit.com/r/aww/top/.json?t=week
+  - https://www.reddit.com/r/aww/top/.json?t=month
+  - https://www.reddit.com/r/aww/top/.json?t=year
+  - https://www.reddit.com/r/aww/top/.json?t=all
+
+- To get pagination to work, you need to add options to the URL.
+  - For example: https://www.reddit.com/r/aww/top/.json?t=year&count=25&after=t3_9fcjig
+  - Before is the ID for the first post in the set. You would use this to go to the previous page
+  - After is the ID for the last post in the set. You would use this to go to the next page
+  - You never use Before and After at the same time
+  - Count is the number of posts you have already seen. By default the json file contains 25 posts, so you would want to increment this by 25 time for each new after request, but for before requests you want to add one.
+    - For example, going from page 1 to 2 you would have count be 25. Going from page 2 to 3 count would be 50, but going from page 2 to 1 would be 26.
+
 ## Technologies
 
 Project built with:
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+let count = 25;
+let after = "";
+
+const getSubredditUrl = input => {
+  let sub = input.val();
+  let url = "https://www.reddit.com/r/" + sub + "/top.json?t=all";
+  return url;
+};
+
 const formSubmit = event => {
   event.preventDefault();
-  const searchInput = $("#searchForm")
-  let sub = searchInput.val();
-  let url = "https://www.reddit.com/r/" + sub + "/hot.json?jsonp=?";
+  let searchInput = $("#searchForm");
+  let url = getSubredditUrl(searchInput);
   $.getJSON(url, function(data) {
-    let html = `<div class="columns">`;
+    after = data.data.after;
+    let html = `<div class="columns" id="posts">`;
     $.each(data.data.children, function(i, item) {
       let img = "";
       if (
@@ -16,7 +25,7 @@ const formSubmit = event => {
           </div>`;
       }
       html += `
-        <div class="column col-lg-12 col-xl-6 col-6 mb-2"">
+        <div class="column col-lg-12 col-xl-6 col-6 mb-2 singlePost" id="${item.data.id}">
           <div class="card">
             ${img}
             <div class="card-body">
@@ -29,8 +38,50 @@ const formSubmit = event => {
         </div>`;
     });
     $("#app").html(html);
-    searchInput.val('');
   });
+  // searchInput.val('');
 }
 
 $("#subSearch").on("submit", formSubmit);
+
+const loadMoreData = () => {
+  console.log("previous: " + after);
+  let searchInput = $("#searchForm");
+  let url = "https://www.reddit.com/r/" + searchInput.val() + "/top/.json?t=all&after=" + after;
+  console.log(url);
+  $.getJSON(url, function(data) {
+    after = data.data.after;
+    console.log("new: " + after);
+    let html = "";
+    $.each(data.data.children, function(i, item) {
+      let img = "";
+      if (
+        item.data.url &&
+        item.data.url.match(/\.(jpg|png|jpeg|bpm|gif)$/)
+      ) {
+        img = `<div class="card-image">
+          <img src="${item.data.url}" class="img-responsive" />
+          </div>`;
+      }
+      html += `
+        <div class="column col-lg-12 col-xl-6 col-6 mb-2 singlePost" id="${item.data.id}">
+          <div class="card">
+            ${img}
+            <div class="card-body">
+              <span class="card-title">${item.data.title}</span>
+            </div>
+            <div class="card-footer">
+              <a href="https://www.reddit.com${item.data.permalink}" target="_blank">Permalink</a>
+            </div>
+          </div>
+        </div>`;
+    });
+    $("#posts").append(html);
+  });
+};
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+    loadMoreData(after);
+  }
+});
