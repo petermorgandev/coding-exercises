893252e933928956a3bf17bee1a7d16b116558ca
Petermorgangh <peter@1080peter.com>
2019-06-22 14:12:56 -0600

    Moved content type matching to seperate function

--- app.js
+++ app.js
@@ -18,28 +18,35 @@ let state = {
       this.url += `&after=${this.after}`
     })
   },
+  contentEmbed: function(url){
+    if (
+      url &&
+      url.match(/\.(jpg|png|jpeg|bpm|gif)$/)
+    ) {
+      content = `<div class="card-image">
+        <a href="${url}"><img src="${url}" class="img-responsive" /></a>
+        </div>`;
+      return content;
+    } 
+    else if (
+      url &&
+      url.match(/\.(gifv)$/)
+    ) {
+      let newURL = url.replace(".gifv", ".mp4");
+      content = `<div class="card-image">
+      <video src="${newURL}" style="max-width: 100%" autoplay loop />
+      </div>`;
+      return content;
+    }
+    else {
+      return "";
+    }
+  },
   formatPosts: function(){
     let html = `<div class="columns">`;
     $.each(this.posts, function(i, item) {
       let {url, id, title, permalink} = item.data;
-      let content = "";
-      if (
-        url &&
-        url.match(/\.(jpg|png|jpeg|bpm|gif)$/)
-      ) {
-        content = `<div class="card-image">
-          <a href="${url}"><img src="${url}" class="img-responsive" /></a>
-          </div>`;
-      } 
-      else if (
-        url &&
-        url.match(/\.(gifv)$/)
-      ) {
-        let newURL = url.replace(".gifv", ".mp4");
-        content = `<div class="card-image">
-        <video src="${newURL}" style="max-width: 100%" autoplay loop />
-        </div>`;
-      }
+      let content = state.contentEmbed(url);
       html += `
         <div class="column col-lg-12 col-xl-6 col-6 mb-2 singlePost" id="${id}">
           <div class="card">
