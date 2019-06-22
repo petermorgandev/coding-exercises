3acd77512b3efdf4e5afa10944b4312a828fd811
Petermorgangh <peter@1080peter.com>
2019-06-22 13:09:54 -0600

    Starting to move code into objects

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-let count = 25;
-let after = "";
-
-const getSubredditUrl = input => {
-  let sub = input.val();
-  let url = "https://www.reddit.com/r/" + sub + "/top.json?t=all";
-  return url;
+let state = {
+  after: '',
+  posts: [],
+  searchInput: '',
+  url: '',
+  getSubredditUrl: function(){
+    this.url = `https://www.reddit.com/r/${this.searchInput.val()}/top.json?t=all`;
+  },
+  getPosts: async function(){
+    await fetch(this.url)
+    .then(res => res.json())
+    .then(json => {
+      let {after, children} = json.data;
+      this.after = after;
+      this.posts = children;
+      this.url += `&after=${this.after}`
+    })
+  }
 };
 
-const formSubmit = event => {
+const formSubmit = async event => {
   event.preventDefault();
-  let searchInput = $("#searchForm");
-  let url = getSubredditUrl(searchInput);
-  $.getJSON(url, function(data) {
-    after = data.data.after;
-    let html = `<div class="columns" id="posts">`;
-    $.each(data.data.children, function(i, item) {
-      let img = "";
-      if (
-        item.data.url &&
-        item.data.url.match(/\.(jpg|png|jpeg|bpm|gif)$/)
-      ) {
-        img = `<div class="card-image">
-          <img src="${item.data.url}" class="img-responsive" />
-          </div>`;
-      }
-      html += `
-        <div class="column col-lg-12 col-xl-6 col-6 mb-2 singlePost" id="${item.data.id}">
-          <div class="card">
-            ${img}
-            <div class="card-body">
-              <span class="card-title">${item.data.title}</span>
-            </div>
-            <div class="card-footer">
-              <a href="https://www.reddit.com${item.data.permalink}" target="_blank">Permalink</a>
-            </div>
-          </div>
+  state.searchInput = $("#searchForm");
+  state.searchInput.blur();
+  state.getSubredditUrl();
+  state.getPosts();
+  let html = `<div class="columns" id="posts">`;
+  $.each(state.posts, function(i, item) {
+    let {url, id, title, permalink} = item.data;
+    let img = "";
+    if (
+      url &&
+      url.match(/\.(jpg|png|jpeg|bpm|gif)$/)
+    ) {
+      img = `<div class="card-image">
+        <img src="${url}" class="img-responsive" />
         </div>`;
-    });
-    $("#app").html(html);
+    }
+    html += `
+      <div class="column col-lg-12 col-xl-6 col-6 mb-2 singlePost" id="${id}">
+        <div class="card">
+          ${img}
+          <div class="card-body">
+            <span class="card-title">${title}</span>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.reddit.com${permalink}" target="_blank">Permalink</a>
+          </div>
+        </div>
+      </div>`;
   });
+  $("#app").html(html);
   // searchInput.val('');
 }
 
