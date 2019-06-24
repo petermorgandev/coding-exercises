2bf99527c2f92dfebdb9abb798585d74ea0f784b
Petermorgangh <peter@1080peter.com>
2019-06-23 21:16:24 -0600

    Small refactor

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const getTextarea = document.getElementById("textarea");
 
 const addLI = isNested => {
+  event.target.blur();
   const splitArray = getTextarea.value.split("\n");
-  const getSelect = document.getElementById("indentation");
-  const { value } = getSelect;
+  const { value } = document.getElementById("indentation");
   return splitArray.map(x => isNested ? `${value}<li>${x}</li>` : `<li>${x}</li>`);
-
 };
 
 const nestList = (arr, mode) => [`<${mode}>`, ...arr, `</${mode}>`];
@@ -14,21 +13,14 @@ const appendLineBreaks = inputArray => inputArray.join("\r\n");
 
 const basicList = () => {
   getTextarea.value = appendLineBreaks(addLI(false));
-  document.getElementById("liOnly").blur();
 };
 
-const ulList = () => {
-  getTextarea.value = appendLineBreaks(nestList(addLI(true), "ul"));
-  document.getElementById("ulLi").blur();
-};
-
-const olList = () => {
-  getTextarea.value = appendLineBreaks(nestList(addLI(true), "ol"));
-  document.getElementById("olLi").blur();
+const nestedList = () => {
+  getTextarea.value = appendLineBreaks(nestList(addLI(true), event.target.id));
 };
 
 document.getElementById("liOnly").addEventListener("click", basicList);
 
-document.getElementById("ulLi").addEventListener("click", ulList);
+document.getElementById("ul").addEventListener("click", nestedList);
 
-document.getElementById("olLi").addEventListener("click", olList);
+document.getElementById("ol").addEventListener("click", nestedList);
