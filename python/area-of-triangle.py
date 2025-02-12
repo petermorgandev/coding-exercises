c2542ec67f08c7993b20add999291fae3f3bc981
Peter Morgan <peter@petermorgan.dev>
2025-02-12 09:40:35 -0500

    Create area-of-triangle.py

---
+++ python/area-of-triangle.py
@@ -0,0 +1,4 @@
+base = raw_input('What is the base of the triangle? ')
+height = raw_input('What is the height of the triangle? ')
+area = float(base) * float (height) / 2
+print "Area: " + str(area)
